d9419502931ac6289cad4c16a9ac7d2f70ec887f
kamiya-tips <kamiya.tips@gmail.com>
2015-09-27 18:42:01 +0800

    修改比较器bug

--- Assets/DND4/Script/Game/InitiativeComparer.cs
+++ Assets/DND4/Script/Game/InitiativeComparer.cs
@@ -5,25 +5,16 @@ public class InitiativeComparer : IComparer<GameUnit>
 {
 	public int Compare (GameUnit x, GameUnit y)
 	{
-		if (x.Initiative > y.Initiative) {
-			return 1;
-		} else if (x.Initiative == y.Initiative) {
-			if (x.Template.Initiative > y.Template.Initiative) {
-				return 1;
-			} else if (x.Template.Initiative == y.Template.Initiative) {
-				if (x.Index > y.Index) {
-					return 1;
-				} else if (x.Index > y.Index) {
-					//never execute this
-					return 0;
-				} else {
-					return -1;
-				}
+		int init = x.Initiative.CompareTo (y.Initiative);
+		if (init == 0) {
+			int initT = x.Template.Initiative.CompareTo (y.Template.Initiative);
+			if (initT == 0) {
+				return x.Index.CompareTo (y.Index);
 			} else {
-				return -1;
+				return -initT;
 			}
 		} else {
-			return -1;
+			return -init;
 		}
 	}
 }
