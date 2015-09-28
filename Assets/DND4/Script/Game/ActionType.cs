31dae7ccc0f4d0c06b4c349e39d5f93824be25ac
kamiya-tips <kamiya.tips@gmail.com>
2015-09-28 15:17:41 +0800

    文件整理

--- Assets/DND4/Script/Game/ActionType.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public enum ActionType
-{
-	STANDARD,
-	MOVE,
-	MINOR,
-}
--- Assets/DND4/Script/Game/RechargeType.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public enum RechargeType
-{
-	ATWILL,
-	ENCOUNTER,
-	DAILY,
-}
